7b2ef520efdf6dc0230881503fd2a3290dc27654
zira-el <niltonmenezes@live.com>
2021-11-22 17:11:07 -0300

    Adicionar novos itens a coleçao

--- Lista-de-Convidados/index.js
+++ Lista-de-Convidados/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
 const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto"];
 
 app.get("/convidados", (req, res) => {
   if(req.query.nome){
     const {nome} = req.query;
-    const achei = convidados.find(pessoa => pessoa.toLowerCase() === nome.toLowerCase());
+    const existe = convidados.find(pessoa => pessoa.toLowerCase() === nome.toLowerCase());
     
-    if(achei){
+    if(existe){
       res.json("Convidado presente.");
       return;
     }
@@ -19,7 +21,21 @@ app.get("/convidados", (req, res) => {
   }
 
   res.json(convidados);
-})
+});
+
+app.post("/convidados", (req, res) => {
+  const {nome} = req.body;
+  const existe = convidados.find(pessoa => pessoa.toLowerCase() === nome.toLowerCase());
+
+  if(existe){
+    res.json("O nome do convidado a ser adicionado já existe na lista. Caso queria adicionar outro convidado de mesmo nome, favor fornecer o sobrenome também.")
+    return
+  }
+
+  convidados.push(nome);
+  res.json("Convidado adicionado com sucesso");
+  return;
+});
 
 app.listen(8001);
 
