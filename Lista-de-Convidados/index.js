8f65af0bdb09fe912df1cb720d983c8157e1d4a0
zira-el <niltonmenezes@live.com>
2021-11-22 16:37:48 -0300

    First commit: Testando get

---
+++ Lista-de-Convidados/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto"];
+
+app.get("/convidados", (req, res) => {
+  res.json(convidados);
+})
+
+app.listen(8001);
+
