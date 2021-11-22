2b0604bfc455e4b94b8a41fd9dafc7cccd05f389
zira-el <niltonmenezes@live.com>
2021-11-22 16:54:47 -0300

    Retornar apensas um item da coleçao

--- Lista-de-Convidados/index.js
+++ Lista-de-Convidados/index.js
@@ -5,6 +5,19 @@ const app = express();
 const convidados = ["Carlos", "Amanda", "Fernanda", "Juliana", "Lucas", "Roberto"];
 
 app.get("/convidados", (req, res) => {
+  if(req.query.nome){
+    const {nome} = req.query;
+    const achei = convidados.find(pessoa => pessoa.toLowerCase() === nome.toLowerCase());
+    
+    if(achei){
+      res.json("Convidado presente.");
+      return;
+    }
+
+    res.json("O convidado buscado não está presente na lista.");
+    return;
+  }
+
   res.json(convidados);
 })
 
