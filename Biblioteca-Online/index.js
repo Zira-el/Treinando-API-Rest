12d904f1e2f4b440f1109727a95b8e8f28dbc116
zira-el <niltonmenezes@live.com>
2021-11-23 16:42:07 -0300

    Biblioteca Online

---
+++ Biblioteca-Online/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "A Odisséia de Jonas",
+        autor: "Thomas Crawling",
+        ano: 2001,
+        numPaginas: 197
+    },
+    {
+        id: 2,
+        titulo: "Jonas e a sociedade escondida",
+        autor: "Claire Crawling",
+        ano: 2004,
+        numPaginas: 158
+    }
+];
+
+app.listen(8001);
