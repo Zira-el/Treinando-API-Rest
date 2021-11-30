840a8ea80516a71b183e6e945c88e37ffe440b90
zira-el <niltonmenezes@live.com>
2021-11-30 15:25:08 -0300

    Retornando lista de pokemons

--- Assicronia/Poke-API/controllers/retornarPokemon.js
+++ Assicronia/Poke-API/controllers/retornarPokemon.js
@@ -1,9 +1,13 @@
+const axios = require("axios");
+
 async function retornarListaDePokemons(req, res) {
-  return
+  const { inicio, quantidade } = req.query;
+  const pokemons = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${inicio}&limit=${quantidade}`);
+  res.json(pokemons.data.results);
 }
 
 async function retornarUmPokemon(req, res) {
-  return
+
 }
 
 module.exports = {
