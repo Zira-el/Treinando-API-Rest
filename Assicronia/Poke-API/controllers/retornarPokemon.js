28431e465a6a49a8f151262fea38b0738289da95
zira-el <niltonmenezes@live.com>
2021-11-30 16:23:40 -0300

    Validaçao de query

--- Assicronia/Poke-API/controllers/retornarPokemon.js
+++ Assicronia/Poke-API/controllers/retornarPokemon.js
@@ -2,6 +2,11 @@ const axios = require("axios");
 
 async function retornarListaDePokemons(req, res) {
   const { inicio, quantidade } = req.query;
+
+  if (!Number(inicio) && inicio != '0' || !Number(quantidade) & quantidade != 0) {
+    return res.status(400).json("Os valores de 'inicio' e 'quantidade' devem ser números.");
+  }
+
   const pokemons = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${inicio}&limit=${quantidade}`);
   res.json(pokemons.data.results);
 }
